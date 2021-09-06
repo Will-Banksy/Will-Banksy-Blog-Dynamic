35a7b6cd8760794f75ebc331e8036e6cc30a2e90
William Banks <williamjb555@gmail.com>
2021-09-06 19:37:05 +0100

    Added some much needed security - Only admin can export now.

--- server.js
+++ server.js
@@ -32,13 +32,32 @@ app.post("/admin/make-post", function(request, response) {
 	}
 
 	if(isAdmin) {
-		response.send("Hello *Admin*! Function is not yet available I am most afraid, but I shall export this glitch project to your github repo anyway.")
+		response.send("Hello *Admin*! Function is not yet available I am most afraid. My sincerest apologies for the inconvenience.")
 	} else {
-		response.send("Hello! Sorry function currently unavailable. Shall export to github anyways")
+		response.send("[ERROR]: Action requires admin");
+	}
+});
+
+app.post("/admin/export", function(request, response) {
+	let isAdmin = false;
+	if(request.body) {
+		console.log(JSON.stringify(request.body));
+		if(request.body.adminPass) {
+			let passHash = sha256(request.body.adminPass);
+			console.log("[Login Attempt]: pass=" + request.body.adminPass + ", hash=" + passHash);
+			if(sha256(request.body.adminPass) === process.env.ADMIN_KEY) {
+				isAdmin = true;
+			}	
+		}
 	}
 
-	// Export to glitch branch on github. Captured request from clicking "Export to GitHub" button
-	// exec("curl 'https://api.glitch.com/project/githubExport?projectId=8985f063-d158-49b5-809b-df1e680346fc&repo=Will-Banksy%2FWill-Banksy-Blog-Dynamic&commitMessage=Export+From+Glitch' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0' -H 'Accept: */*' -H 'Accept-Language: en-GB,en;q=0.5' --compressed -H 'Referer: https://glitch.com/' -H 'Authorization: 07ef9923-1761-4a85-ab8e-904efe903712' -H 'Origin: https://glitch.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data-raw ''")
+	if(isAdmin) {
+		// Export to glitch branch on github. Captured request from clicking "Export to GitHub" button
+		exec("curl 'https://api.glitch.com/project/githubExport?projectId=8985f063-d158-49b5-809b-df1e680346fc&repo=Will-Banksy%2FWill-Banksy-Blog-Dynamic&commitMessage=Export+From+Glitch' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0' -H 'Accept: */*' -H 'Accept-Language: en-GB,en;q=0.5' --compressed -H 'Referer: https://glitch.com/' -H 'Authorization: 07ef9923-1761-4a85-ab8e-904efe903712' -H 'Origin: https://glitch.com' -H 'DNT: 1' -H 'Connection: keep-alive' -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' --data-raw ''")
+		response.send("Exporting to github...");
+	} else {
+		response.send("[ERROR]: Action requires admin");
+	}
 });
 
 // Listen on a port
